2c2ed7102c3c062755b93a682edba62f1243146a
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2022-11-17 19:04:54 +0100

    finish Controller and SimpleGUI, missing SimpleGUIWhitFileChooser

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,53 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    /* point 1 */
+    private static final String PATH = System.getProperty("user.home") + File.separator
+    + "output.txt";
+    private File curFile = new File(PATH);
+
+    /* point 5 */
+    public void setFile(final File file) {
+        final File daddy = file.getParentFile();
+        if(daddy.exists()) {
+            curFile = file;
+        } else {
+            throw new IllegalArgumentException("Cannot save the file in a non-existing folder");
+        }
+    }
+
+    /* caso in cui ci da la stringa, cioè il path del file*/
+    public void setFile(final String filepath) {
+        setFile(new File(filepath));
+    }
+
+    /* point 2 */
+    public File getCurrentFile() {
+        return curFile;
+    }
+
+    /* point 3 */
+    public String getCurrentFilePath() {
+        return curFile.getPath();
+    }
+
+    /* point 4 */
+    public void save(final String text) throws IOException {
+        try (PrintStream output = new PrintStream(curFile, StandardCharsets.UTF_8)) {
+            output.println(text);
+        }
+    }
+
+    
+
+    
 }
--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,17 @@
 package it.unibo.mvc;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +20,46 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private final Integer PROPORTION = 4;
+
+    public SimpleGUI() {
+        final Controller controller = new Controller();
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextArea txtArea = new JTextArea();
+        
+        final JButton save = new JButton("Save");
+        save.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    controller.save(txtArea.getText());
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage(), "there was an error", JOptionPane.ERROR_MESSAGE);
+                }                
+            }            
+        });
+        
+        canvas.add(txtArea, BorderLayout.CENTER);
+        canvas.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+
+    }
+
+    private void display() {
+        frame.setVisible(true);
+    }
+
+    public static void main(String... args) {
+        new SimpleGUI().display();
+    }
 
 }
